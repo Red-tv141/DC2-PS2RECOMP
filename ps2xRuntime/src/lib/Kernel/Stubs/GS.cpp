96ec0b114e1721f484e18e76416b4645024a0f41
Red-tv141 <101547056+Red-tv141@users.noreply.github.com>
2026-07-24 18:16:43 +0300

    Add files via upload

--- ps2xRuntime/src/lib/Kernel/Stubs/GS.cpp
+++ ps2xRuntime/src/lib/Kernel/Stubs/GS.cpp
@@ -1,3 +1,4 @@
+// G359 recompile-bump v2 (MSBuild misses .inc-dependency changes; edit this .cpp to force it).
 #include "GS_parts/gs_stub_crt_and_config.inc"
 #include "GS_parts/gs_stub_sync_and_interrupts.inc"
 #include "GS_parts/gs_stub_draw_env_and_packets.inc"
